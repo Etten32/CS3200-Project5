95656683600ecb570660700aa482f45bd0040dc3
Etten32 <etten327vcr@gmail.com>
2022-04-04 15:34:31 -0400

    Added testHarness.cpp

---
+++ testHarness.cpp
@@ -0,0 +1,12 @@
+//J+M+J
+// TEST HARNESS TO PROBE TRIE CLASS METHOD FUNCTIONALITY
+
+#include "Trie.h"
+
+using namespace std;
+
+int main(){
+    // test constructor
+    Trie* testTrie0 = new Trie();
+    testTrie0->outputTrie(cout);
+}
